07ae6cddf524d6679de4fdf40c4dc23e39c84a0a
Rahul <rahulyerme1997@gmail.com>
2021-07-20 15:32:06 +0530

    second commit docker.java new file

---
+++ src/main/java/mavendocker/Docker.java
@@ -0,0 +1,10 @@
+package mavendocker;
+
+public class Docker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Changes...!");
+	}
+
+}
---
+++ src/main/java/mavendocker/mavendocker.java
@@ -0,0 +1,11 @@
+package mavendocker;
+
+public class mavendocker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Changes");
+
+	}
+
+}
